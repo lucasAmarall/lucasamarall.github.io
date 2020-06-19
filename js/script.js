593eb3da5f39b78f9a80180e68fe335e84679120
Lucas Amaral <lucas.amaral@nodo.cc>
2020-06-18 21:21:55 -0300

    ...

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
-function setPhotos(){
-    document.querySelector('.intro__image').setAttribute('src', '../images/lucas.jpg');
-    document.querySelector('.hero').style.backgroundImage = "url(../images/hero.jpg)";
-    document.querySelector('.testimonial').style.backgroundImage = "url(../images/testimonial-bg.jpg)";
+function loadImage(imagePath) {
+  var protocol = window.location.protocol || 'http';
+  var host = window.location.host;
+  var url = protocol + "//" + host + imagePath
+  return new Promise(res => {
+      fetch(url).then(r => r.blob()).then(blob => {
+        var reader = new FileReader();
+        reader.onload = function() {
+          var b64 = reader.result.replace(/^data:.+;base64,/, '');
+          res("data:image/png;base64," + b64)
+        };
+        reader.readAsDataURL(blob);
+      });
+    })
+    // return fetch(url+ imagePath, {dataType:"image/jpg",})
+}
+
+function setPhotos() {
+  var hero = document.querySelector('.hero');
+  var testimonial = document.querySelector('.testimonial');
+  var introPhoto = document.querySelector('.intro__image');
+
+  loadImage('/images/lucas.jpg').then(function(res) { introPhoto.setAttribute("src", res) })
+  loadImage('/images/hero.jpg').then(function(res) { hero.style.backgroundImage = "url("+ res +")" })
+  loadImage('/images/testimonial-bg').then(function(res) { testimonial.style.backgroundImage = "url("+ res +")" })
 }
 
 window.addEventListener('load', setPhotos)
