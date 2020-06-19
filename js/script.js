4da581effd284a7dc19938fc4c407a69052f9bed
Lucas Amaral <lucas.amaral@nodo.cc>
2020-06-19 16:35:31 -0300

    ...

--- js/script.js
+++ js/script.js
@@ -3,16 +3,15 @@ function loadImage(imagePath) {
   var host = window.location.host;
   var url = protocol + "//" + host + imagePath
   return new Promise(res => {
-      fetch(url).then(r => r.blob()).then(blob => {
-        var reader = new FileReader();
-        reader.onload = function() {
-          var b64 = reader.result.replace(/^data:.+;base64,/, '');
-          res("data:image/png;base64," + b64)
-        };
-        reader.readAsDataURL(blob);
-      });
-    })
-    // return fetch(url+ imagePath, {dataType:"image/jpg",})
+    fetch(url).then(r => r.blob()).then(blob => {
+      var reader = new FileReader();
+      reader.onload = function() {
+        var b64 = reader.result.replace(/^data:.+;base64,/, '');
+        res("data:image/png;base64," + b64)
+      };
+      reader.readAsDataURL(blob);
+    });
+  })
 }
 
 function setPhotos() {
@@ -21,8 +20,22 @@ function setPhotos() {
   var introPhoto = document.querySelector('.intro__image');
 
   loadImage('/images/lucas.jpg').then(function(res) { introPhoto.setAttribute("src", res) })
-  loadImage('/images/hero.jpg').then(function(res) { hero.style.backgroundImage = "url("+ res +")" })
-  loadImage('/images/testimonial-bg').then(function(res) { testimonial.style.backgroundImage = "url("+ res +")" })
+  loadImage('/images/hero.jpg').then(function(res) { hero.style.backgroundImage = "url(" + res + ")" })
+  loadImage('/images/testimonial-bg.jpg').then(function(res) { testimonial.style.backgroundImage = "url(" + res + ")" })
 }
 
-window.addEventListener('load', setPhotos)
+function initIntroSwiper(){
+  new Swiper ('.swiper-container', {
+    loop: true,
+    // If we need pagination
+    pagination: {
+      el: '.about-swiper-pagination',
+    }
+  })
+}
+function init() {
+  // setPhotos();
+  initIntroSwiper()
+}
+
+window.addEventListener('load', init)
